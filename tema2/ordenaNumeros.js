9571af386a109373058287bee93eb7e2c8265f3c
fran-dawbaza <fgargar092@g.educaand.es>
2025-10-06 12:48:52 +0200

    sep 06b

--- tema2/ordenaNumeros.js
+++ tema2/ordenaNumeros.js
@@ -4,7 +4,11 @@ formulario.addEventListener('submit', muestraOrdenados);
 function muestraOrdenados(event) {
     event.preventDefault();
     /* capturar los números  y ordenarlos */
- 
-    /* mostrar el resultado en el HTML en el párrafo con id="resultado" */
-    
+    let num1 = +document.getElementById('num1').value;
+    let num2 = +document.getElementById('num2').value;
+    let num3 = +document.getElementById('num3').value;
+
+    /* mostrar el resultado en el HTML en el párrafo con id="result" */
+    let parrafo = document.getElementById('result');
+    parrafo.innerHTML = 'muestra los números ordenados aquí';
 }
