b1688153d06fb9de0c2108ca60eb295e7b5aaad5
fran-dawbaza <fgargar092@g.educaand.es>
2025-10-06 12:10:08 +0200

    oct 06

---
+++ tema1/algo.js
@@ -0,0 +1,23 @@
+let year = prompt('¿En qué año fue publicada la especificación ECMAScript-2015?', '');
+
+if (year == 2015){
+    alert( '¡Lo adivinaste, correcto!' );
+}       
+else if (year == 2016)  {
+        alert( '¡te has pasado por poco!' )
+}    
+else {
+        alert( '¿Cómo puedes estar tan equivocado?' )
+}
+
+
+if (year == 2015)       alert( '¡Lo adivinaste, correcto!' );
+else if (year == 2016)  alert( '¡te has pasado por poco!' );
+else                    alert( '¿Cómo puedes estar tan equivocado?' );
+
+if (year == 2015)       
+    alert( '¡Lo adivinaste, correcto!' );
+else if (year == 2016)  
+    alert( '¡te has pasado por poco!' );
+else                    
+    alert( '¿Cómo puedes estar tan equivocado?' );
---
+++ tema2/ordenaNumeros.js
@@ -0,0 +1,10 @@
+let formulario = document.getElementById('formulario');
+formulario.addEventListener('submit', muestraOrdenados);
+
+function muestraOrdenados(event) {
+    event.preventDefault();
+    /* capturar los números  y ordenarlos */
+ 
+    /* mostrar el resultado en el HTML en el párrafo con id="resultado" */
+    
+}
