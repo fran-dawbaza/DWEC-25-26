08be2a3b9cd0862bda73348e3f13fad2eb80742f
fran-dawbaza <fgargar092@g.educaand.es>
2026-01-14 09:56:00 +0100

    ene 14 3

--- tema3/js/delegacionEventos.js
+++ tema3/js/delegacionEventos.js
@@ -1,31 +1,36 @@
-let contador = 4;
+document.addEventListener('DOMContentLoaded', main);
 
-// 1. Seleccionamos el elemento PADRE
-const lista = document.getElementById('lista-tareas');
+function main(){
 
-// 2. Agregamos UN SOLO evento al padre
-lista.addEventListener('click', evento => {
+    let contador = 4;
 
-    // 3. Detectamos quién originó el click (event delegation)
-    // Verificamos si el elemento clickeado tiene la clase 'btn-borrar'
-    console.log(evento.target);        // Elemento más interno
-    console.log(evento.currentTarget); // Elemento que programa el evento
-    if (evento.target.dataset.accion==='borrar') {
-        // Buscamos el elemento <li> padre del botón para eliminarlo
-        const tarea = evento.target.closest('li');
-        console.log('Tarea eliminada mediante delegación:' + tarea.textContent);
-        tarea.remove();
-        
-    }
-    else if (evento.target.tagName==='LI'){
-        console.log('Tarea eliminada mediante delegación:' + evento.target.textContent);
-        evento.target.remove();
-    }
-});
+    // 1. Seleccionamos el elemento PADRE
+    const lista = document.getElementById('lista-tareas');
 
-// --- Código extra para demostrar que funciona con elementos nuevos ---
-document.getElementById('btn-agregar').addEventListener('click', () => {
-    const li = document.createElement('li');
-    li.innerHTML = `Tarea Nueva ${contador++} <button data-accion="borrar">Eliminar</button>`;
-    lista.append(li);
-});
+    // 2. Agregamos UN SOLO evento al padre
+    lista.addEventListener('click', evento => {
+
+        // 3. Detectamos quién originó el click (event delegation)
+        // Verificamos si el elemento clickeado tiene la clase 'btn-borrar'
+        console.log(evento.target);        // Elemento más interno
+        console.log(evento.currentTarget); // Elemento que programa el evento
+        if (evento.target.dataset.accion==='borrar') {
+            // Buscamos el elemento <li> padre del botón para eliminarlo
+            const tarea = evento.target.closest('li');
+            console.log('Tarea eliminada mediante delegación:' + tarea.textContent);
+            tarea.remove();
+            
+        }
+        else if (evento.target.tagName==='LI'){
+            console.log('Tarea eliminada mediante delegación:' + evento.target.textContent);
+            evento.target.remove();
+        }
+    });
+
+    // --- Código extra para demostrar que funciona con elementos nuevos ---
+    document.getElementById('btn-agregar').addEventListener('click', () => {
+        const li = document.createElement('li');
+        li.innerHTML = `Tarea Nueva ${contador++} <button data-accion="borrar">Eliminar</button>`;
+        lista.append(li);
+    });
+}
